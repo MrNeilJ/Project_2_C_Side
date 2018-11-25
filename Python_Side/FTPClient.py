0717292e5f3e3c2aaf21927a318c761e846d1874
MrNeilJ <mr.neil.j@gmail.com>
2018-11-25 15:02:19 -0800

    Migrated files into separate folders to allow for proper searching to occur

--- Python_Side/FTPClient.py
+++ Python_Side/FTPClient.py
@@ -45,7 +45,7 @@ def connect_server():
    
     clientSocket = socket(AF_INET,SOCK_STREAM)              
     
-    clientSocket.connect((serverName, sys.argv[2]))
+    clientSocket.connect((serverName, int(sys.argv[2])))
     
     return clientSocket
 
